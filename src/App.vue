678cc6f918be83fe85513091f37f930194eeaea0
Michael Rodriguez <maiikol1192@gmail.com>
2021-11-03 06:35:09 -0500

    cambios

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
-<template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+<template >
+  <div class="h-screen w-full ">
+    <router-view></router-view>
   </div>
-  <router-view />
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  font-family: 'Rubik', 'Quicksand', Helvetica, Arial, sans-serif;
+  background: #D0D0D0;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+html{
+font-size: 62.5%;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,26 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHashHistory } from "vue-router";
+import Home from "../views/Inicio.vue";
+import Precios from "../views/Precios.vue";
 
 const routes = [
   {
     path: "/",
-    name: "Home",
+    name: "",
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+    path: "/precios", 
+    name: "precios",
+    component: Precios,
+  }
 ];
 
+
+
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
+  mode: 'history' // Eliminar el signo #
 });
 
 export default router;
